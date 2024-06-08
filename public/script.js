852df3ef5da96d5f8d38df34dc69cdd2573aea30
bethany1293 <bethanyphoon@gmail.com>
2024-06-09 01:57:56 +1000

    Update index.html and script.js
    Removed unnecessary features from HTML including add recipe log and side bar

--- public/script.js
+++ public/script.js
@@ -10,9 +10,3 @@ document.getElementById('add-recipe-btn').addEventListener('click', function(){
     let addRecipeDialog = document.querySelector("#add-recipe-dialog"); //stores and selects the modal form to 'add a recipe'
     addRecipeDialog.showModal() //displays the modal form named 'addRecipeDialog'
 });
-
-//When the 'add a recipe log' button is clicked, display the modal form named 'addRecipeDialog' on-screen
-document.getElementById('add-recipe-log-btn').addEventListener('click', function(){
-    let addRecipeLogDialog = document.querySelector("#add-recipe-log-dialog"); //stores and selects the modal form to 'add a recipe log'
-    addRecipeLogDialog.showModal() //displays the modal form named 'addRecipeLogDialog'
-});
