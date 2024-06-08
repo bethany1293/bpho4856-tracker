3839f69404906d44fbe71e8155c1de358b7dbb49
bethany1293 <bethanyphoon@gmail.com>
2024-06-08 19:39:25 +1000

    Added modal form for 'Add a recipe log' w/ HTML & JS
    Used addeventlistener with a function to display the modal window when 'add a recipe log' is pressed in JS. Added the form inputs roughly in HTML and part of the SCSS styling from the 'add a recipe' form

--- public/script.js
+++ public/script.js
@@ -5,11 +5,14 @@ function RecipeCard(name, image){
     this.recipeImage = image;
 }
 
-//Selects the dialog element which is a form to 'add a recipe' and stores it as a variable
-let addRecipeDialog = document.querySelector("dialog");
-
-
-//When the 'add a recipe' button is clicked, show the modal form named 'addRecipeDialog'
+//When the 'add recipe' button is clicked, display the modal form named 'addRecipeDialog' on-screen
 document.getElementById('add-recipe-btn').addEventListener('click', function(){
-    addRecipeDialog.showModal()
+    let addRecipeDialog = document.querySelector("#add-recipe-dialog"); //stores and selects the modal form to 'add a recipe'
+    addRecipeDialog.showModal() //displays the modal form named 'addRecipeDialog'
+});
+
+//When the 'add a recipe log' button is clicked, display the modal form named 'addRecipeDialog' on-screen
+document.getElementById('add-recipe-log-btn').addEventListener('click', function(){
+    let addRecipeLogDialog = document.querySelector("#add-recipe-log-dialog"); //stores and selects the modal form to 'add a recipe log'
+    addRecipeLogDialog.showModal() //displays the modal form named 'addRecipeLogDialog'
 });
