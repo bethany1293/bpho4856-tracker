12c47cb6679cb6eea59fc863db5d44655c315a65
bethany1293 <bethanyphoon@gmail.com>
2024-06-09 23:47:28 +1000

    Removed 'add a recipe log' and navbar
    Removed existing functionalities out of scope including 'add a recipe log'. Removed the navigation bar as it would be an SPA, with the 'recipe logs' no longer existing. Added 'add recipe', 'delete recipe', 'view recipe' functionalities using HTML, JS and SCSS styling. Made the website mobile responsive using media queries. Updated README.me

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ## Project Overview
 
-BakeTrove is a single-page application (SPA), which is designed for hobby bakers and aspiring professional bakers who want to log their dessert recipes and track their baking attempts, to act as a learning journal and recipe organiser. This app was built using HTML, SCSS and vanilla JavaScript. It uses 'localStorage' for data storage and retrieval, and Node.JS with Express.JS for a simple web server.
+BakeTrove is a single-page application (SPA), which is designed for hobby bakers and aspiring professional bakers who want to log their dessert recipes and track their baking attempts to act as a recipe organiser. This app was built using HTML, SCSS and vanilla JavaScript. It uses 'localStorage' for data storage and retrieval, and Node.JS with Express.JS for a simple web server. The project also uses Bootstrap and Jquery as CDNs to use Summernote, a simple WYSIWYG editor, enabling formatting of text. The Summernote JS library was useful for users to format recipe ingredients with an unordered list and the recipe steps with an ordered list. Furthermore, the FileReader API was used to enable image upload of dessert recipes.
 
 ## Version Control
 
@@ -10,58 +10,81 @@ This project uses Git and GitHub to track changes and to manage different change
 
 ## Features
 
+- Add a recipe 
+
+- Mobile responsive using media queries and flexbox
+
 ## Setup
 
-Download the latest version of Node.JS. (Express.JS, Parcel, SCSS setup (???))
+1. Download Node.JS (if it is not already installed on your device)
+
+2. Download the ZIP file called 'bpho4856-tracker' for this project.
+
+3. Open the project folder in Visual Studio Code (VS Code), then type 'npm install' in the terminal of VS code to install all the necessary dependencies for this project
 
 ## Usage
 
-1. Download the ZIP file called 'bpho4856-tracker' for this repository in GitHub.
+1. Open the project folder in Visual Studio Code (VS Code).
 
-2. Open the project folder in Visual Studio Code (VS Code).
+2. In the in-built terminal of VS Code, type 'npm run dev' to start the server.
 
-3. In the in-built terminal of VS Code, type 'npm run dev'.
+3. Copy the server link that is provided to you in the terminal e.g. http://localhost:1234.
 
-4. Copy the server link that is provided to you in the terminal.
+4. Paste the server link into your preferred browser to access and use the web app.
 
-5. Paste the server link into your preferred browser to access the web app.
+Note1: Sometimes parcel throws an error, in which case '.parcel-cache' needs to be deleted.
+Note2: The folder in '/assets/images-to-test-with' can be used to test the recipe image upload function when adding a recipe.
 
 ## Limitations
 
-## Style References
+Local storage was used instead of a full database, so the web app was limited by having a maximum data storage limit. Also, as this web app is an minimum viable product (MVP), the range of functionalities is currently limited to only adding recipes, viewing recipes, and deleting recipes. In the future, more functionality that was previously planned, would be added to increase the scope of the web app to my previously targeted use case.
 
-### Colours
+## Original High-Fidelity Wireframe - Plan
 
-### Typefaces
+<img src="assets/designs/home-page-desktop.png" alt="drawing" width="600"/>
 
-## Prototype vs Mockup changes
-
-Navigation Bar:
-
-- Increased the size of the hamburger menu icon from 25px (~1.5625em) to 3em (~48px) for accessibility reasons, visibility and to match the logo font size for visual balance.
-
-- Increased the spacing between the 'BakeTrove' logo font and the hamburger icon from 19px (~1.1875em) to 1.5em (~24px).
+Originally the development
 
 ## Reflection
 
-### Challenges
-
-### What I would do differently
-
 ### Future Steps
 
-## Supported Browsers
-
 ## References List (APA 7th)
+
+Adams, V. (n.d.). *Nunito*. <https://fonts.google.com/specimen/Nunito?query=Nunito>
+
+Bell, A. (2019). *A Modern CSS Reset.* <https://dev.to/hankchizljaw/a-modern-css-reset-6p3>
 
 Chen, N. (2023). *Japanese Cheesecake (Video) スフレチーズケーキ*. [Photo]. <https://www.justonecookbook.com/souffle-japanese-cheesecake/>
 
 Clarkson, E. (2023). *30 minute Chewy Chocolate Cookies*. [Photo]. <https://cloudykitchen.com/blog/chocolate-cookies/>
 
-Icons8. (n.d). *Cooking Book.* [Clip Art]. <https://icons8.com/icon/idEQE7TsG0NA/cooking-book>
+Coyier, C. (2024). *A Complete Guide to Flexbox*. <https://css-tricks.com/snippets/css/a-guide-to-flexbox/>
 
-Icons8. (n.d). *Dashboard Layout*. [Clip Art]. <https://icons8.com/icon/aVHe2jHuORcA/dashboard-layout>
+Dongas, R. (n.d.). *Upload image to base64*. <https://codepen.io/robdongas/pen/dyggepJ>
 
-Icons8. (n.d). *List View*. [Clip Art]. <https://icons8.com/icon/95645/list-view>
+Duckworth, E. (2022). *How To Make A Swiss Roll Cake*. <https://emmaduckworthbakes.com/how-to-make-a-swiss-roll-cake/>
 
-Lee, M. (2021). *Kahlua Buns Stuffed W/ Mascarpone Cream And Salted Pistacchio Powder*. [Photo]. <https://ladyandpups.com/2021/12/08/kahlua-buns-stuffed-w-mascarpone-cream-and-salted-pistacchio-powder/>
+[@Felix Jr]. (2018). *how to open multiple modal bootstrap with different id*. <https://stackoverflow.com/questions/53904558/how-to-open-multiple-modal-bootstrap-with-different-id>
+
+Giraudel, K. (n.d.). *An opinionated styleguide for writing sane, maintainable and scalable Sass*. https://sass-guidelin.es/
+
+Karin. (2016). *How to write variable with multiple css properties in Sass.* <https://stackoverflow.com/questions/38968129/how-to-write-variable-with-multiple-css-properties-in-sass>
+
+[@mrkvon]. (2022). *@use not working in Sass to import variables but @import is working.* <https://stackoverflow.com/questions/73172387/use-not-working-in-sass-to-import-variables-but-import-is-working>
+
+Nagi. (2023). *Crème Brûlée (French vanilla custard)*. <https://www.recipetineats.com/creme-brulee/>
+
+Rambeck, N. (2017). *BEM by Example*. <https://sparkbox.com/foundry/bem_by_example>
+
+Roffey, M. (2021). *A Complete Guide to Flexbox*. <https://www.sugarsaltmagic.com/custard-danish-pastry/>
+
+Ryu, Y. (n.d.). *Dongle*. <https://fonts.google.com/specimen/Dongle?query=Dongle>
+
+Sass Team. (n.d.). *Sass Basics*. <https://sass-lang.com/guide/#topic-6>
+
+Summernote Team. (n.d.). *Getting Started*. <https://summernote.org/getting-started/>
+
+[@underscore]. (2014). *Get the data-target dynamically*. <https://stackoverflow.com/questions/23590918/get-the-data-target-dynamically>
+
+Web Dev Simplified Blog. (2023). *Modals Will Never Be The Same - HTML dialog Element*. <https://blog.webdevsimplified.com/2023-04/html-dialog/>
--- public/script.js
+++ public/script.js
@@ -1,12 +1,167 @@
-let recipeCards = [];
-
-function RecipeCard(name, image){
-    this.recipeName = name;
-    this.recipeImage = image;
-}
-
 //When the 'add recipe' button is clicked, display the modal form named 'addRecipeDialog' on-screen
 document.getElementById('add-recipe-btn').addEventListener('click', function(){
     let addRecipeDialog = document.querySelector("#add-recipe-dialog"); //stores and selects the modal form to 'add a recipe'
-    addRecipeDialog.showModal() //displays the modal form named 'addRecipeDialog'
+    addRecipeDialog.showModal(); //displays the modal form named 'addRecipeDialog'
+    localStorage["recipe-name"] =""; 
+    localStorage["recipe-category"] = "";
+    localStorage["ingredients"] ="";
+    localStorage["method"] = "";
+    localStorage["imgData"] = "";
+    localStorage["baking-techniques"]="";
 });
+
+document.getElementById('cancel-btn').addEventListener('click', function(){
+    let addRecipeDialog = document.querySelector("#add-recipe-dialog"); //stores and selects the modal form to 'add a recipe'
+    addRecipeDialog.close(); // Closes the dialog
+});
+
+//This function is to delete  the template, already-existing recipes
+$('.del-recipe').on('click', function(){
+    if (confirm("Are you sure you want to delete this recipe?") == true) {
+        let recipeDelete = $(this).attr("data-target");
+        document.getElementById(recipeDelete).innerHTML = " ";
+        document.getElementById(recipeDelete).remove();
+    }
+});
+
+//This function is to delete  the dynamically generated new recipes
+$(document).on('click', '.del-recipedyn', function(){
+    if (confirm("Are you sure you want to delete this recipe?") == true) {
+        let recipeDelete = $(this).attr("data-target");
+        document.getElementById(recipeDelete).innerHTML = " ";
+        document.getElementById(recipeDelete).remove();
+    }
+});
+
+document.querySelector('form').onsubmit = function(e) {
+    e.preventDefault();
+    let recipeName = document.querySelector('#recipe-name').value;
+    let recipeCategory = document.querySelector('#recipe-category').value;
+    let ingredients = document.querySelector('#ingredients').value;
+    let method = document.querySelector('#method').value;
+    let bakingTechniques = document.querySelector('#baking-techniques').value;
+    localStorage["recipe-name"] = recipeName; 
+    localStorage["recipe-category"] = recipeCategory;
+    localStorage["ingredients"] = ingredients;
+    localStorage["method"] = method;
+    localStorage["baking-techniques"] = bakingTechniques;
+    let modalName = recipeName.replace(" ", "-");
+    modalName= modalName.substring(0,8);
+    let recipeId = modalName + "-recipe";
+    console.log(recipeId);
+    let el= document.getElementById('recipes');
+    let newrecipe = `<article id="`;
+    newrecipe+= recipeId;
+    newrecipe+=`" class="recent-recipes--card">
+                                <img src="`
+        newrecipe+= localStorage["imgData"];
+        newrecipe+=`"/>
+                                    <section class="recent-recipes--card-text">
+                                    <p><a href="#" data-target="#`;
+        newrecipe+= modalName;
+        newrecipe+= `" data-toggle="modal">`;
+        newrecipe+= localStorage["recipe-name"];
+        newrecipe+= `</a></p> <button class="del-recipedyn" data-target="`;
+        newrecipe+= recipeId;
+        newrecipe+=`">Delete</button>
+
+                                </section>
+
+                                <div class="modal fade" id="`;
+        newrecipe+= modalName;
+        newrecipe+=`" style="z-index:9999999;"  role="dialog">
+                                    <div class="modal-dialog">
+                                        <div class="modal-content">
+                                            <div class="modal-header">`;
+        newrecipe+= localStorage["recipe-name"];
+        newrecipe+=`</div>
+                                            <div class="modal-body">
+                                                <img src="`;
+        newrecipe+=localStorage["imgData"];
+        newrecipe+=`"/>
+                                                <h2>Ingredients</h2>`;
+        newrecipe+=localStorage["ingredients"];
+        newrecipe+=`
+                                                <h2>Method</h2>`;
+        newrecipe+=localStorage["method"];
+        newrecipe+=`                                                <h2>Category</h2>
+                                                <p>`;
+        newrecipe+=localStorage["recipe-category"];
+        newrecipe+=`</p>
+                                                <h2>Baking Techniques</h2>
+                                                <p>`;
+        newrecipe+=localStorage["baking-techniques"];
+        newrecipe+=`</p>
+                                            </div>
+                                            <div class="modal-footer">
+                                                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </article>`;
+    el.insertAdjacentHTML("afterbegin", newrecipe);
+    let addRecipeDialog = document.querySelector("#add-recipe-dialog"); //stores and selects the modal form to 'add a recipe'
+    addRecipeDialog.close(); // Closes the dialog
+  }
+
+  // Get the image input and destination elements
+const imgInput = document.getElementById("img-input");
+const imgDest = document.getElementById("img-dest");
+
+// Add a 'change' event listener to the image input element
+imgInput.addEventListener("change", function (event) {
+  // Create a new FileReader instance
+  var reader = new FileReader();
+
+  // Get the first selected file from the input event (the image)
+  var selectedFile = event.target.files[0];
+
+  // Set up the FileReader's 'onloadend' event handler
+  reader.onloadend = function (e) {
+    // Get the base64 representation of the image from the event target result
+    var base64 = e.target.result;
+    
+    // Log the base64 data to the console
+    console.log(base64);
+    
+    // Store the base64 image data in localStorage with the key 'imgData'
+    localStorage.setItem("imgData", base64);
+    
+    // Set the destination element's src attribute to the base64 data to display the uploaded image
+    imgDest.src = base64;
+  };
+
+  // Read the uploaded image as a Data URL (Base64 encoded string)
+  reader.readAsDataURL(selectedFile);
+});
+
+//Summernote function
+$(document).ready(function() {
+    $('#ingredients').summernote();
+    $('#method').summernote();
+  });
+
+  $('#ingredients').summernote({
+    toolbar: [
+      // [groupName, [list of button]]
+      ['style', ['bold', 'italic', 'underline', 'clear']],
+      ['font', ['strikethrough', 'superscript', 'subscript']],
+      ['fontsize', ['fontsize']],
+      ['color', ['color']],
+      ['para', ['ul', 'ol', 'paragraph']],
+      ['height', ['height']]
+    ]
+  });
+
+  $('#method').summernote({
+    toolbar: [
+      // [groupName, [list of button]]
+      ['style', ['bold', 'italic', 'underline', 'clear']],
+      ['font', ['strikethrough', 'superscript', 'subscript']],
+      ['fontsize', ['fontsize']],
+      ['color', ['color']],
+      ['para', ['ul', 'ol', 'paragraph']],
+      ['height', ['height']]
+    ]
+  });
